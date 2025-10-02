fe1275f78f634639885109b931c43466cb8c1158
dudis <ihenkeandersen@gmail.com>
2025-10-02 12:34:15 +0200

    Common lib

---
+++ GeoQuiz/CommonModels/QuizCreationModels/BaseModels/CreateQuizBaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonModels.QuizCreationModels.BaseModels
+{
+    public class CreateQuizBaseClass
+    {
+        public QuizType QuizType { get; set; }
+        public QuizCategory Category { get; set; }
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ GeoQuiz/CommonModels/QuizCreationModels/QuizManual/CreateTriviaQuiz.cs
@@ -0,0 +1,20 @@
+﻿using CommonModels.QuizCreationModels.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonModels.QuizCreationModels.QuizManual
+{
+    public class CreateTriviaQuiz : CreateQuizBaseClass
+    {
+        public List<TriviaQuizQuestion> Questions { get; set; } = new();
+    }
+
+    public class TriviaQuizQuestion
+    {
+        public string Question { get; set; } = "";
+        public List<string> ValidAnswers { get; set; } = new();
+    }
+}
