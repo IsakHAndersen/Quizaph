c9f42a4bd559830da46d43d3d419b6ca0706dc2d
Isak <ihenkeandersen@gmail.com>
2025-10-13 12:45:56 +0200

    appsettings

--- GeoQuiz/QuizaphBackend/obj/Debug/net8.0/QuizaphBackend.AssemblyInfo.cs
+++ GeoQuiz/QuizaphBackend/obj/Debug/net8.0/QuizaphBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizaphBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4eab1aefde9d1940126681e942b247ce7bd2d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d20a7768fb297c09f6da5557df44c7fe6d8a37")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizaphBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizaphBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
